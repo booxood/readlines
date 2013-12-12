36da8c8ac9855ca0cd54e6d8fe52721522c8f796
Liucw <279674457@qq.com>
2013-12-12 21:49:55 +0800

    fix

--- test/read.js
+++ test/read.js
@@ -9,69 +9,75 @@ var fiveFilePath = path.join(__dirname, 'file/five.txt');
 var fiveWindowsFilePath = path.join(__dirname, 'file/five_windows.txt');
 var spFilePath = path.join(__dirname, 'file/sp.txt');
 
-describe('sync read file line as array', function(){
-    before(function(){
-    });
-
-    it('should return null array, read a empty file', function(){
-        var lines = fd.readlinesSync(nullFilePath);
-        lines.should.be.an.instanceof(Array).and.have.lengthOf(0);
-    });
-
-    it('should return array of length equal 5, read five.txt', function(){
-        var lines = fd.readlinesSync(fiveFilePath);
-        lines.should.be.an.instanceof(Array).and.have.lengthOf(5);
-        lines[4].should.equal('5');
-    });
-
-    it('should return array of length equal 5, read five_windows.txt', function(){
-        var lines = fd.readlinesSync(fiveWindowsFilePath);
-        lines.should.be.an.instanceof(Array).and.have.lengthOf(5);
-        lines[4].should.equal('5');
-    });
-
-    it('should return array of length equal 1, read sp.txt', function(){
-        var lines = fd.readlinesSync(spFilePath);
-        lines.should.be.an.instanceof(Array).and.have.lengthOf(1);
-    });
-
-});
-
-describe('async read file line as array', function(){
-    before(function(done){
-        done();
-    });
-
-    it('should return null array, read a empty file', function(done){
-        fd.readlines(nullFilePath, function(err, lines){
-            lines.should.be.an.instanceof(Array).and.have.lengthOf(0);
-            done();
+describe('sync case', function(){
+    
+    describe('sync read file line as array', function(){
+        before(function(){
         });
-    });
 
-    it('should return array of length equal 5, read five.txt', function(done){
-        fd.readlines(fiveFilePath, function(err, lines){
+        it('should return null array, read a empty file', function(){
+            var lines = fd.readlinesSync(nullFilePath);
+            lines.should.be.an.instanceof(Array).and.have.lengthOf(0);
+        });
+
+        it('should return array of length equal 5, read five.txt', function(){
+            var lines = fd.readlinesSync(fiveFilePath);
             lines.should.be.an.instanceof(Array).and.have.lengthOf(5);
             lines[4].should.equal('5');
-            done();
         });
-    });
-});
 
-describe('sync read a line', function(){
-    it('should return 3, read the second line', function(){
-        var line = fd.readlineSync(fiveFilePath, 3);
-        line.should.equal('3');
-    });
-});
+        it('should return array of length equal 5, read five_windows.txt', function(){
+            var lines = fd.readlinesSync(fiveWindowsFilePath);
+            lines.should.be.an.instanceof(Array).and.have.lengthOf(5);
+            lines[4].should.equal('5');
+        });
 
-describe('async read a line', function(){
-    it('should return 3, read the second line', function(done){
-        fd.readline(fiveFilePath, 3, function(err, line){
-            should.not.exist(err);
+        it('should return array of length equal 1, read sp.txt', function(){
+            var lines = fd.readlinesSync(spFilePath);
+            lines.should.be.an.instanceof(Array).and.have.lengthOf(1);
+        });
+
+    });
+
+    describe('sync read a line', function(){
+        it('should return 3, read the three line', function(){
+            var line = fd.readlineSync(fiveFilePath, 3);
             line.should.equal('3');
+        });
+    }); 
+});
+
+describe('async case', function(){
+
+    describe('async read file line as array', function(){
+        before(function(done){
             done();
         });
 
+        it('should return null array, read a empty file', function(done){
+            fd.readlines(nullFilePath, function(err, lines){
+                lines.should.be.an.instanceof(Array).and.have.lengthOf(0);
+                done();
+            });
+        });
+
+        it('should return array of length equal 5, read five.txt', function(done){
+            fd.readlines(fiveFilePath, function(err, lines){
+                lines.should.be.an.instanceof(Array).and.have.lengthOf(5);
+                lines[4].should.equal('5');
+                done();
+            });
+        });
     });
+
+    describe('async read a line', function(){
+        it('should return 3, read the three line', function(done){
+            fd.readline(fiveFilePath, 3, function(err, line){
+                should.not.exist(err);
+                line.should.equal('3');
+                done();
+            });
+        });
+    });
+
 });
