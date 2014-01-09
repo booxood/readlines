d64b026a5c39a3b36a8e1c2885f1b0b9043b3a11
liucw <279674457@qq.com>
2014-01-09 11:22:22 +0800

    Add return null when read no thing

--- test/read.js
+++ test/read.js
@@ -10,7 +10,7 @@ var fiveWindowsFilePath = path.join(__dirname, 'file/five_windows.txt');
 var spFilePath = path.join(__dirname, 'file/sp.txt');
 
 describe('sync case', function(){
-    
+
     describe('sync read file line as array', function(){
         before(function(){
         });
@@ -40,11 +40,16 @@ describe('sync case', function(){
     });
 
     describe('sync read a line', function(){
-        it('should return 3, read the three line', function(){
+        it('should return 3, read the 3 line', function(){
             var line = fd.readlineSync(fiveFilePath, 3);
             line.should.equal('3');
         });
-    }); 
+
+        it('should return null, read the -1 line', function(){
+            var line = fd.readlineSync(fiveFilePath, -1);
+            should.strictEqual(null, line);
+        });
+    });
 });
 
 describe('async case', function(){
@@ -71,10 +76,19 @@ describe('async case', function(){
     });
 
     describe('async read a line', function(){
-        it('should return 3, read the three line', function(done){
+        it('should return 3, read the 3 line', function(done){
             fd.readline(fiveFilePath, 3, function(err, line){
                 should.not.exist(err);
                 line.should.equal('3');
+                done();
+            });
+        });
+
+        it('should return null, read the 6 line', function(done){
+            fd.readline(fiveFilePath, 6, function(err, line){
+                should.not.exist(err);
+                console.log(line);
+                should.strictEqual(null, line);
                 done();
             });
         });
